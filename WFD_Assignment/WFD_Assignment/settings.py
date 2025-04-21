1825f112f4d034e527088a2747984a13e15a1371
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2025-04-21 23:43:32 +0100

    Added bootstrap & Created a basic non-functional login screen

--- WFD_Assignment/WFD_Assignment/settings.py
+++ WFD_Assignment/WFD_Assignment/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Assignment.apps.AssignmentConfig"
+    "Assignment.apps.AssignmentConfig",
 ]
 
 MIDDLEWARE = [
